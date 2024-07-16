e5f7c73b51df7fb6b47462dcc13bfca76ac7a17c
KANNAN.J <113454965+kannan304@users.noreply.github.com>
2024-07-16 09:36:05 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import "./Style.css"
+const API_KEY = process.env.API_KEY;
 
 function Home() {
   const [messages, setMessages] = useState([]);
